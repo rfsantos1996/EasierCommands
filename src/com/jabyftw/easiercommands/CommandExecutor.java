2d73a5a20cc3e125aa683b7eb5f6b6dd4fda1ea3
Rafael <rfsantos1996@globo.com>
2015-03-28 21:03:37 -0300

    Fixed somethings here
    Now you can see where to edit to get the Player class

--- src/com/jabyftw/easiercommands/CommandExecutor.java
+++ src/com/jabyftw/easiercommands/CommandExecutor.java
@@ -9,12 +9,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.logging.Logger;
 
 /**
  * Created by Rafael on 03/03/2015.
  */
 public class CommandExecutor implements org.bukkit.command.CommandExecutor {
+
+    private static final boolean DEBUG = false;
+    private static final Class<?> PLAYER_CLASS = Jogador.class; // TODO change here for your custom player class
+    private static String TITLE = "";
+
+    private static final Logger logger = Bukkit.getLogger();
+
+    static {
+        for(int i = 0; i < 7; i++) {
+            TITLE += '=';
+        }
+    }
 
     protected CommandExecutor(final JavaPlugin plugin, final String name, final String description, final String usageMessage) {
         final org.bukkit.command.CommandExecutor executor = this;
@@ -35,15 +51,17 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] stringArguments) {
         if(command.getPermission() != null && command.getPermission().length() > 0 && !commandSender.hasPermission(command.getPermission())) {
-            commandSender.sendMessage(command.getPermissionMessage());
+            commandSender.sendMessage(getColoredMessage(command.getPermissionMessage()));
             return true;
         }
 
+        if(DEBUG) logger.info(TITLE);
+        if(DEBUG) logger.info("Command used: " + stringArguments.length + " arguments -> /" + label + " " + Arrays.toString(stringArguments));
         Argument[] objectArguments = new Argument[stringArguments.length];
         {
             for(int i = 0; i < stringArguments.length; i++) {
                 objectArguments[i] = ArgumentType.handleArgument(commandSender, stringArguments[i]);
-                //main.getLogger().info("* i=" + i + "/" + stringArguments.length + " " + objectArguments[i].toString());
+                if(DEBUG) logger.info("Handled argument: " + (i + 1) + "/" + stringArguments.length + ": " + objectArguments[i].toString());
             }
         }
 
@@ -52,32 +70,35 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
         LinkedList<Object> objects = new LinkedList<>();
 
         for(Method currentMethod : getClass().getDeclaredMethods()) {
+            if(DEBUG) logger.info(TITLE.replaceAll("=", "*"));
             if(currentMethod.isAnnotationPresent(CommandHandler.class)) {
 
                 CommandHandler declaredAnnotation = currentMethod.getDeclaredAnnotation(CommandHandler.class);
                 String additionalPermission = declaredAnnotation.additionalPermission();
 
-                if(additionalPermission.length() == 0 || commandSender.hasPermission(additionalPermission)) {
+                if(declaredAnnotation.senderType().canHandleCommandSender(commandSender) && (additionalPermission.length() == 0 || commandSender.hasPermission(additionalPermission))) {
                     Class<?>[] requiredArguments = currentMethod.getParameterTypes();
 
                     boolean isPlayerNeeded;
 
                     if(requiredArguments.length > 0 && currentMethod.getReturnType().isAssignableFrom(HandleResponse.class) &&
-                            ((isPlayerNeeded = requiredArguments[0].isAssignableFrom(Player.class)) || requiredArguments[0].isAssignableFrom(CommandSender.class))) {
+                            ((isPlayerNeeded = requiredArguments[0].isAssignableFrom(PLAYER_CLASS)) || requiredArguments[0].isAssignableFrom(CommandSender.class))) {
 
                         {
                             LinkedList<Object> objectList = new LinkedList<>();
 
                             if(objectArguments.length < (requiredArguments.length - 1)) {
+                                if(DEBUG) logger.info(currentMethod.getName() + " -> not enough arguments");
                                 continue; // Not enough arguments
                             }
 
-                            Player player = null;
+                            Object player = null;
 
                             if(isPlayerNeeded && !(commandSender instanceof Player)) {
+                                if(DEBUG) logger.info(currentMethod.getName() + " -> player is incompatible again");
                                 continue; // Can't handle this command on console
                             } else if(isPlayerNeeded) {
-                                player = (Player) commandSender;
+                                player = getPlayerClass(commandSender); // Change here the method to get the player class
                             }
 
                             {
@@ -86,6 +107,7 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
 
                             Integer integer = doLoop(objectList, requiredArguments, objectArguments);
 
+                            if(DEBUG) logger.info(currentMethod.getName() + " -> ** RESULT: " + integer + "/" + nearValue + " **");
                             if(integer != null && integer <= nearValue) {
                                 mostNear = currentMethod;
                                 nearValue = integer;
@@ -93,28 +115,37 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
                                 objects.clear();
                                 objects.addAll(objectList);
 
-                                if(integer == 0) break; // Break method-iterator
+                                if(DEBUG) logger.info(currentMethod.getName() + " -> ** most near method **");
+                                if(integer == 0) {
+                                    if(DEBUG) logger.info(currentMethod.getName() + " -> ** perfect method: " + getMethodArguments(currentMethod.getParameterTypes()) + " **");
+                                    break; // Break method-iterator
+                                }
                             } else {
                                 objectList.clear();
                             }
                         }
-                    }
-                }
+                    } else if(DEBUG) logger.info(currentMethod.getName() + " -> player is incompatible with first argument or method is invalid (" + PLAYER_CLASS.getName() + ")");
+                } else if(DEBUG) logger.info(currentMethod.getName() + " -> Player is incompatible or don't have permission");
             }
         }
+        if(DEBUG) logger.info(TITLE.replaceAll("=", "*"));
 
         if(mostNear == null) {
-            commandSender.sendMessage(command.getUsage());
+            commandSender.sendMessage(getColoredMessage(command.getUsage()));
+            if(DEBUG) logger.info("Processing: -> no method available");
         } else {
             try {
+                if(DEBUG)
+                    logger.info("Processing: -> invoking method: " + mostNear.getName() + " (" + getMethodArguments(mostNear.getParameterTypes()) + " -> " + getMethodArguments(getClassesFromList(objects)) + ") " +
+                            "with " + objects.size() + "/" + mostNear.getParameterCount() + " arguments");
                 HandleResponse handleResponse = (HandleResponse) mostNear.invoke(this, objects.toArray());
 
                 switch(handleResponse) {
                     case RETURN_HELP:
-                        commandSender.sendMessage(command.getUsage());
+                        commandSender.sendMessage(getColoredMessage(command.getUsage()));
                         break;
                     case RETURN_NO_PERMISSION:
-                        commandSender.sendMessage(command.getPermissionMessage());
+                        commandSender.sendMessage(getColoredMessage(command.getPermissionMessage()));
                         break;
                 }
 
@@ -123,7 +154,38 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
             }
         }
 
+        if(DEBUG) logger.info(TITLE);
         return true;
+    }
+
+    private Object getPlayerClass(CommandSender commandSender) {
+        return (Player) commandSender; // TODO change here for your custom class
+    }
+
+    private String getMethodArguments(Class<?>... objects) {
+        String[] parametersName = new String[objects.length];
+
+        for(int i = 0; i < objects.length; i++) {
+            parametersName[i] = objects[i].getSimpleName();
+        }
+
+        return Arrays.toString(parametersName);
+    }
+
+    private Class<?>[] getClassesFromList(Collection<Object> objects) {
+        Class<?>[] classes = new Class<?>[objects.size()];
+
+        Iterator<Object> iterator = objects.iterator();
+        int size = objects.size();
+        int index = 0;
+
+        while(iterator.hasNext() && size >= 0) {
+            classes[index] = iterator.next().getClass();
+            index++;
+            size--;
+        }
+
+        return classes;
     }
 
     private Integer doLoop(LinkedList<Object> objectList, Class<?>[] requiredArguments, Argument[] objectArguments) {
@@ -168,5 +230,11 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
         }
 
         return objectArguments.length - objectIndex; // 0 when all arguments were used, > 0 when there are remaining arguments
+    }
+
+    /* Some Util imported from my projects */
+
+    public static String getColoredMessage(String original) {
+        return ChatColor.translateAlternateColorCodes('&', original);
     }
 }
